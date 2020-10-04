9b277a7757d1518b39e13bd259bfa3906483e5cf
Jim Le <taki.lnt@gmail.com>
2020-10-04 00:12:08 -0700

    Add main activity rearrange student part

---
+++ TugOfLogic/app/src/main/java/specialtopic/groupfive/tugoflogic/MainActivity.kt
@@ -0,0 +1,25 @@
+package specialtopic.groupfive.tugoflogic
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import specialtopic.groupfive.tugoflogic.instructor.InstructorMainActivity
+import specialtopic.groupfive.tugoflogic.student.StudentMainActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    public fun onStudentBtnClick(view: View) {
+        val intent = Intent(this, StudentMainActivity::class.java)
+        startActivity(intent)
+    }
+
+    public fun onInstructorBtnClick(view: View) {
+        val intent = Intent(this, InstructorMainActivity::class.java)
+        startActivity(intent)
+    }
+}
