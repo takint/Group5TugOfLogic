ab8239ffdbe5d161ef1635c04ef2eae7a54adb33
chaungng <43162908+chaungng@users.noreply.github.com>
2020-11-28 22:17:48 -0800

    add NewMainClaimActivity

---
+++ TugOfLogic/app/src/main/java/specialtopic/groupfive/tugoflogic/instructor/ui/instructormainclaim/AddNewMainClaimActivity.kt
@@ -0,0 +1,12 @@
+package specialtopic.groupfive.tugoflogic.instructor.ui.instructormainclaim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import specialtopic.groupfive.tugoflogic.R
+
+class AddNewMainClaimActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_main_claim)
+    }
+}
--- TugOfLogic/app/src/main/java/specialtopic/groupfive/tugoflogic/instructor/ui/instructormainclaim/InstructorMainClaimFragment.kt
+++ TugOfLogic/app/src/main/java/specialtopic/groupfive/tugoflogic/instructor/ui/instructormainclaim/InstructorMainClaimFragment.kt
@@ -3,22 +3,36 @@ package specialtopic.groupfive.tugoflogic.instructor.ui.instructormainclaim
 import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.system.Os.open
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_game_room.*
+import kotlinx.android.synthetic.main.fragment_instructor_mainclaim.*
 import specialtopic.groupfive.tugoflogic.R
 import specialtopic.groupfive.tugoflogic.instructor.adapters.InstructorMainClaimsRVAdapter
+import specialtopic.groupfive.tugoflogic.instructor.ui.gameroom.ChooseMainClaimActivity
 import specialtopic.groupfive.tugoflogic.roomdb.DataRepository
 import specialtopic.groupfive.tugoflogic.roomdb.entities.MainClaim
+import java.io.InputStream
+import java.lang.Exception
+
+import com.github.nkzawa.emitter.Emitter
+import com.github.nkzawa.socketio.client.IO
+import com.github.nkzawa.socketio.client.Socket
+import specialtopic.groupfive.tugoflogic.WEB_SERVICE_URL
+import java.nio.channels.AsynchronousFileChannel.open
 
 
 class InstructorMainClaimFragment : Fragment() {
@@ -35,7 +49,10 @@ class InstructorMainClaimFragment : Fragment() {
     ): View? {
         instructorMainClaimViewModel =
             ViewModelProviders.of(this).get(InstructorMainClaimViewModel::class.java)
+        // Fragment Root
         val root = inflater.inflate(R.layout.fragment_instructor_mainclaim, container, false)
+
+        // Fragment title
         val textView: TextView = root.findViewById(R.id.text_instructor_main_claim_title)
 
         instructorMainClaimViewModel.text.observe(viewLifecycleOwner, Observer {
@@ -63,5 +80,16 @@ class InstructorMainClaimFragment : Fragment() {
                 mMainClaimsRV.layoutManager = mLayoutManager
             })
         }
+
+        // Add Floating Action Button - FAB
+        fab_instructor.setOnClickListener(View.OnClickListener {
+            Log.i(TAG, "onViewCreated: Add Button Clicked! Go to NewMainClaim Activity")
+
+            val addNewMainClaimIntent = Intent(activity, AddNewMainClaimActivity::class.java).apply {  }
+            startActivity(addNewMainClaimIntent)
+
+            // Skip the MainClaim Fragment and go back to the parent activity
+            activity?.onBackPressed()
+        })
     }
 }
